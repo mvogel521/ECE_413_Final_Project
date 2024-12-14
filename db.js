2137586274520907681356669f3eebfed8588f52
mvogel521 <mileeavogel123@gmail.com>
2024-12-13 23:41:34 -0700

    Version and Heart Rate Fixes

--- db.js
+++ db.js
@@ -1,9 +1,6 @@
 // to use mongoDB
 const mongoose = require("mongoose");
-mongoose.set('strictQuery', true);
+
 mongoose.connect("mongodb://127.0.0.1/ece413513MongoDB", { useNewUrlParser: true, useUnifiedTopology:true });
 
-
 module.exports = mongoose;
-
-// mongoose.connect("mongodb://127.0.0.1/authen", { useNewUrlParser: true, useUnifiedTopology:true }); *** line from given source code 
